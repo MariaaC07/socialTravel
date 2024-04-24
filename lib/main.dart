a4c23a919444a5591ca95de8c5dc7afcdc35116c
MariaaC07 <maria-a.cojocaru@student.upt.ro>
2024-04-24 22:09:30 +0300

    setting up firebase

--- lib/main.dart
+++ lib/main.dart
@@ -3,8 +3,14 @@ import 'package:travel/responsive/mobile_screen_layout.dart';
 import 'package:travel/responsive/responsive_layout_screen.dart';
 import 'package:travel/responsive/web_screen_layout.dart';
 import 'package:travel/utils/colors.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main()  async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
