be7b4345cb09a8d0600baf3e26a69cea098d4fb0
MariaaC07 <maria-a.cojocaru@student.upt.ro>
2024-05-16 21:21:06 +0300

    create profile UI screen

--- lib/utils/global_var.dart
+++ lib/utils/global_var.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:travel/screens/add_post_screen.dart';
 import 'package:travel/screens/feed_screen.dart';
+import 'package:travel/screens/profile_screen.dart';
 import 'package:travel/screens/search_screen.dart';
 
 const webScreenSize = 600;
@@ -10,5 +11,5 @@ const homeScreenItems = [
   SearchScreen(),
   AddPostScreen(),
   Text('notif'),
-  Text('profile')
+  ProfileScreen()
 ];
